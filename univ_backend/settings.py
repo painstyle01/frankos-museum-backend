b67bec2cb4c9338e01f9edfc5b0791198f4f66e5
mshelyhin-amplify <mshelyhin@amplify.com>
2022-03-26 11:02:35 +0200

    CRSF enable

--- univ_backend/settings.py
+++ univ_backend/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://frankos-museum-backend.azurewebsites.net"]
