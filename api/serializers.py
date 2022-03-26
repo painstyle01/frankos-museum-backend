bc6c13a49f1d419902c037b57b0200c8e4bbd9d9
mshelyhin-amplify <mshelyhin@amplify.com>
2022-03-26 13:00:57 +0200

    Update view for Product

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ("name", "description", "price", 'url_to_photo', "avaliable")
+        fields = ("id", "name", "description", "price", 'url_to_photo', "avaliable")
