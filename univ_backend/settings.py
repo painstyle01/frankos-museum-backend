992e71d3635f387a6a7e1bd04a28e8956f5f542c
mshelyhin-amplify <mshelyhin@amplify.com>
2022-03-26 10:28:12 +0200

    Azure push

--- univ_backend/settings.py
+++ univ_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +77,11 @@ WSGI_APPLICATION = "univ_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "frankos-museum",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DBNAME"],
+        "HOST": os.environ["DBHOST"],
+        "USER": os.environ["DBUSER"],
+        "PASSWORD": os.environ["DBPASS"],
     }
 }
 
