ae2c27b5ce15c689040d7e9d9615d79770d57112
g1Den1s-it <g1den1splay@gmail.com>
2022-04-23 17:10:54 +0300

    Added contacts to the project

---
+++ contacts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    time = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    contact_us = models.CharField(max_length=300)
+    arrival = models.CharField(max_length=400)
+    
---
+++ contacts/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import ContactListSerializer
+from .models import Contact
+# Create your views here.
+
+class ContactListView(APIView):
+
+    def get(self, request):
+        contact = Contact.objects.last()
+        serializer = ContactListSerializer(contact)
+        return Response({'contact': serializer.data})
