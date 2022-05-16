cefa4bf8db17284102dc50d6f885d20b984173de
Keez <painstyle01@gmail.com>
2022-05-16 15:45:30 +0300

    Added email endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.donate, name='donate'),
-    path('', views.send_email_with_order, name='email')
+    path('email', views.send_email_with_order, name='email')
 ]
